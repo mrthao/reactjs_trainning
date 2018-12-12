5deb9fb1bde38cf9c782f0126409ba1804e5dabb
Zuong thao <zuongthao@gmail.com>
2018-12-12 13:30:35 +0700

    Bai tap 6(final)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,36 +1,78 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
+import React, {Component} from 'react';
+import Home from './HomeComponent';
+import Menu from "./MenuComponent";
+import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent'
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import {DISHES} from "../shared/dishes";
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 
-class Main extends Component {
+import { connect } from 'react-redux';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
+import About from "./AboutComponent";
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      </div>
-    );
-  }
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
 }
 
-export default Main;
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        }
+    }
+
+    render() {
+        const HomePage = () => {
+          return (
+              <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+          );
+        };
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+        };
+		
+		const Aboutus = () => {
+			return (
+				<About leaders={this.state.leaders} />
+			);
+		}
+
+        return (
+            <div className="App">
+                <Header/>
+                <Switch>
+                    <Route path="/home" component={HomePage}/>
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Route exact path="/contactus" component={Contact}/>
+                    <Route exact path="/menu/:dishId" component={DishWithId}/>
+                    <Route exact path="/aboutus" component={Aboutus}/>
+                    <Redirect to="/home"/>
+                </Switch>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+// export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
